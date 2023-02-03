c942fee41f206f48736a19020ab558245dc466b6
Mohammad Ali Khodayari <makhodayari99@gmail.com>
2023-02-03 23:26:49 +0330

    CPA: Accuracy 16/16

--- side_channel_analysis.py
+++ side_channel_analysis.py
@@ -2,18 +2,37 @@ import utilities as utl
 
 
 if __name__ == '__main__':
+    # S-Box
+    s_box = [
+        99 , 124, 119, 123, 242, 107, 111, 197, 48 , 1  , 103, 43 , 254, 215, 171, 118,
+        202, 130, 201, 125, 250, 89 , 71 , 240, 173, 212, 162, 175, 156, 164, 114, 192,
+        183, 253, 147, 38 , 54 , 63 , 247, 204, 52 , 165, 229, 241, 113, 216, 49 , 21 ,
+        4  , 199, 35 , 195, 24 , 150, 5  , 154, 7  , 18 , 128, 226, 235, 39 , 178, 117,
+        9  , 131, 44 , 26 , 27 , 110, 90 , 160, 82 , 59 , 214, 179, 41 , 227, 47 , 132,
+        83 , 209, 0  , 237, 32 , 252, 177, 91 , 106, 203, 190, 57 , 74 , 76 , 88 , 207,
+        208, 239, 170, 251, 67 , 77 , 51 , 133, 69 , 249, 2  , 127, 80 , 60 , 159, 168,
+        81 , 163, 64 , 143, 146, 157, 56 , 245, 188, 182, 218, 33 , 16 , 255, 243, 210,
+        205, 12 , 19 , 236, 95 , 151, 68 , 23 , 196, 167, 126, 61 , 100, 93 , 25 , 115,
+        96 , 129, 79 , 220, 34 , 42 , 144, 136, 70 , 238, 184, 20 , 222, 94 , 11 , 219,
+        224, 50 , 58 , 10 , 73 , 6  , 36 , 92 , 194, 211, 172, 98 , 145, 149, 228, 121,
+        231, 200, 55 , 109, 141, 213, 78 , 169, 108, 86 , 244, 234, 101, 122, 174, 8  ,
+        186, 120, 37 , 46 , 28 , 166, 180, 198, 232, 221, 116, 31 , 75 , 189, 139, 138,
+        112, 62 , 181, 102, 72 , 3  , 246, 14 , 97 , 53 , 87 , 185, 134, 193, 29 , 158,
+        225, 248, 152, 17 , 105, 217, 142, 148, 155, 30 , 135, 233, 206, 85 , 40 , 223,
+        140, 161, 137, 13 , 191, 230, 66 , 104, 65 , 153, 45 , 15 , 176, 84 , 187, 22
+    ]
+
     # File Preparation
-    known_traces, known_plain_text, known_cipher_text = utl.open_files(True, 200, 370000, 40000, 50000)
-    unknown_traces, unknown_plain_text, unknown_cipher_text = utl.open_files(False, 150, 550000, 0, 30000)
+    known_traces, known_plain_text, known_cipher_text = utl.open_files(200, 370000, 40000, 50000)
 
     # DPA Attack
-    known_key_dpa_lb, known_key_dpa_hw = utl.dpa_attack(known_traces, known_plain_text, 200)
-    unknown_key_dpa_lb, unknown_key_dpa_hw = utl.dpa_attack(unknown_traces, unknown_plain_text, 150)
+    known_key_dpa_lb, known_key_dpa_hw = utl.dpa_attack(s_box, known_traces, known_plain_text)
 
     # CPA Attack
+    known_key_cpa_hw = utl.cpa_attack(s_box, known_traces, known_plain_text)
 
     # Results
-    print('Guessed Key For Known Traces & Plaintext:\n')
+    print('Guessed Key From DPA Attack:\n')
     print('Using Last Bit As Indicator for Ones & Zeros:')
     print(known_key_dpa_lb)
     print('Using Hamming Weight As Indicator for Ones & Zeros:')
@@ -21,8 +40,6 @@ if __name__ == '__main__':
 
     print('─' * 150)
 
-    print('Guessed Key For Unknown Traces & Plaintext:\n')
-    print('Using Last Bit As Indicator for Ones & Zeros:')
-    print(unknown_key_dpa_lb)
-    print('Using Hamming Weight As Indicator for Ones & Zeros:')
-    print(unknown_key_dpa_hw)
+    print('Guessed Key From CPA Attack:\n')
+    print('Using Hamming Weight:')
+    print(known_key_cpa_hw)
--- utilities.py
+++ utilities.py
@@ -1,70 +1,33 @@
 import numpy as np
-from tqdm import tqdm
 
 
-def open_files(known, n_traces, len_trace, power_start, power_len):
-    if known:
-        with open('./data/traces-00112233445566778899aabbccddeeff.bin', 'rb') as trace_file:
-            full_traces = np.fromfile(trace_file, np.uint8).reshape(n_traces, len_trace)
-            power_traces = np.zeros((n_traces, power_len))
-            for i in range(n_traces):
-                power_traces[i] = full_traces[i][power_start:power_start + power_len]
+def open_files(n_traces, len_trace, power_start, power_len):
+    with open('./data/traces-00112233445566778899aabbccddeeff.bin', 'rb') as trace_file:
+        full_traces = np.fromfile(trace_file, np.uint8).reshape(n_traces, len_trace)
+        power_traces = np.zeros((n_traces, power_len))
+        for i in range(n_traces):
+            power_traces[i] = full_traces[i][power_start:power_start + power_len]
 
-        with open('./data/plaintext_00112233445566778899aabbccddeeff.txt', 'r') as plain_file:
-            plain_dec = []
-            for row in plain_file.readlines():
-                for hex_dec in row.split():
-                    plain_dec.append(int(hex_dec, 16))
+    with open('./data/plaintext_00112233445566778899aabbccddeeff.txt', 'r') as plain_file:
+        plain_dec = []
+        for row in plain_file.readlines():
+            for hex_dec in row.split():
+                plain_dec.append(int(hex_dec, 16))
 
-        with open('./data/ciphertext_00112233445566778899aabbccddeeff.txt', 'r') as cipher_file:
-            cipher_dec = []
-            for row in cipher_file.readlines():
-                for hex_dec in row.split():
-                    cipher_dec.append(int(hex_dec, 16))
-    else:
-        with open('./data/traces-unknown_key.bin', 'rb') as trace_file:
-            full_traces = np.fromfile(trace_file, np.uint8).reshape(n_traces, len_trace)
-            power_traces = np.zeros((n_traces, power_len))
-            for i in range(n_traces):
-                power_traces[i] = full_traces[i][power_start:power_start + power_len]
-
-        with open('./data/plaintext_unknown_key.txt', 'r') as plain_file:
-            plain_dec = []
-            for row in plain_file.readlines():
-                for hex_dec in row.split():
-                    plain_dec.append(int(hex_dec, 16))
-
-        with open('./data/ciphertext_unknown_key.txt', 'r') as cipher_file:
-            cipher_dec = []
-            for row in cipher_file.readlines():
-                for hex_dec in row.split():
-                    cipher_dec.append(int(hex_dec, 16))
+    with open('./data/ciphertext_00112233445566778899aabbccddeeff.txt', 'r') as cipher_file:
+        cipher_dec = []
+        for row in cipher_file.readlines():
+            for hex_dec in row.split():
+                cipher_dec.append(int(hex_dec, 16))
 
     return power_traces, plain_dec, cipher_dec
 
 
-def dpa_attack(traces, plain_text, n_traces):
-    s_box = [
-        99 , 124, 119, 123, 242, 107, 111, 197, 48 , 1  , 103, 43 , 254, 215, 171, 118,
-        202, 130, 201, 125, 250, 89 , 71 , 240, 173, 212, 162, 175, 156, 164, 114, 192,
-        183, 253, 147, 38 , 54 , 63 , 247, 204, 52 , 165, 229, 241, 113, 216, 49 , 21 ,
-        4  , 199, 35 , 195, 24 , 150, 5  , 154, 7  , 18 , 128, 226, 235, 39 , 178, 117,
-        9  , 131, 44 , 26 , 27 , 110, 90 , 160, 82 , 59 , 214, 179, 41 , 227, 47 , 132,
-        83 , 209, 0  , 237, 32 , 252, 177, 91 , 106, 203, 190, 57 , 74 , 76 , 88 , 207,
-        208, 239, 170, 251, 67 , 77 , 51 , 133, 69 , 249, 2  , 127, 80 , 60 , 159, 168,
-        81 , 163, 64 , 143, 146, 157, 56 , 245, 188, 182, 218, 33 , 16 , 255, 243, 210,
-        205, 12 , 19 , 236, 95 , 151, 68 , 23 , 196, 167, 126, 61 , 100, 93 , 25 , 115,
-        96 , 129, 79 , 220, 34 , 42 , 144, 136, 70 , 238, 184, 20 , 222, 94 , 11 , 219,
-        224, 50 , 58 , 10 , 73 , 6  , 36 , 92 , 194, 211, 172, 98 , 145, 149, 228, 121,
-        231, 200, 55 , 109, 141, 213, 78 , 169, 108, 86 , 244, 234, 101, 122, 174, 8  ,
-        186, 120, 37 , 46 , 28 , 166, 180, 198, 232, 221, 116, 31 , 75 , 189, 139, 138,
-        112, 62 , 181, 102, 72 , 3  , 246, 14 , 97 , 53 , 87 , 185, 134, 193, 29 , 158,
-        225, 248, 152, 17 , 105, 217, 142, 148, 155, 30 , 135, 233, 206, 85 , 40 , 223,
-        140, 161, 137, 13 , 191, 230, 66 , 104, 65 , 153, 45 , 15 , 176, 84 , 187, 22
-    ]
+def dpa_attack(s_box, traces, plain_text):
+    n_traces, _ = traces.shape
     guess_key_lb = []
     guess_key_hw = []
-    for byte in tqdm(range(16)):
+    for byte in range(16):
         power_hypothesis_lb = []
         power_hypothesis_hw = []
         for k in range(256):
@@ -89,3 +52,23 @@ def dpa_attack(traces, plain_text, n_traces):
         guess_key_lb.append(hex(np.argmax(power_hypothesis_lb)))
         guess_key_hw.append(hex(np.argmax(power_hypothesis_hw)))
     return guess_key_lb, guess_key_hw
+
+
+def cpa_attack(s_box, traces, plain_text):
+    n_traces, m_points = traces.shape
+    guess_key = []
+    for byte in range(16):
+        hypothesis = []
+        for k in range(256):
+            hw_list = []
+            correlation = []
+            for n in range(n_traces):
+                s_box_input = plain_text[16 * n + byte] ^ k
+                s_box_output = s_box[s_box_input]
+                hw = np.binary_repr(s_box_output).count('1')
+                hw_list.append(hw)
+            for m in range(m_points):
+                correlation.append(np.corrcoef(hw_list, traces[:, m])[0, 1])
+            hypothesis.append(np.max(correlation))
+        guess_key.append(hex(np.argmax(hypothesis)))
+    return guess_key
